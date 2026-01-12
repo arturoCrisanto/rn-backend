e41dd09c7bfa4f33541d714f14a5f84add8d853e
Ruz Maray <arturocrisanto1@gmail.com>
2026-01-12 14:52:35 +0800

    feat: replace console logging with logger for better error handling in cron job

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,14 +1,19 @@
 import cron from "cron";
 import https from "https";
 import "dotenv/config";
+import { logger } from "../utils/helpers/logger";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("GET request sent successfully");
-      else console.log("GET request failed", res.statusCode);
+      if (res.statusCode === 200) {
+        logger.info("GET request sent successfully");
+      } else
+        logger.error(
+          `Failed to send GET request. Status code: ${res.statusCode}`
+        );
     })
-    .on("error", (e) => console.error("Error while sending request", e));
+    .on("error", (e) => logger.error("Error while sending request", e));
 });
 
 export default job;
